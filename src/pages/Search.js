7a561cdda3ea4e3a7648ff337340c7d8b3971ba2
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-06-07 19:13:40 +0100

    WIP: create home and search pages

---
+++ src/pages/Search.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <button className="close-search"></button>
+          <div className="search-books-input-wrapper">
+            <input type="text" placeholder="Search by title or author" />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid" />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Search
