e1065ceb0131f51245c1d823601e888106dcf693
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-06-07 19:58:58 +0100

    Chore: more cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import * as BooksAPI from './utils/BooksAPI'
 
+// Pages imports
+import Header from './components/Header/Header'
+import Home from './pages/Home'
+import Search from './pages/Search'
+
 class BooksApp extends React.Component {
+  async componentDidMount() {
+    try {
+      const books = await BooksAPI.getAll()
+      console.log('[all books] ->', books)
+    } catch (error) {
+      console.log('[Error: error from gettings books] ->', error.message)
+    }
+  }
   state = {}
 
   render() {
-    return <div className="app"></div>
+    return (
+      <BrowserRouter>
+        <Header />
+        <div className="wrapper">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/search" component={Search} />
+        </div>
+      </BrowserRouter>
+    )
   }
 }
 
