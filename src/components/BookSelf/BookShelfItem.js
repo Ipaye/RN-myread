f2c4279fccbf0cbdc1105a72dbe3370758e881a2
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-06-08 10:26:03 +0100

    UI: Finished first allowable markup for bookshelf item

--- src/components/BookSelf/BookShelfItem.js
+++ src/components/BookSelf/BookShelfItem.js
@@ -4,13 +4,15 @@ import PropTypes from 'prop-types'
 function BookShelfItem(props) {
   return (
     <div className="book">
-      <div className="book-top">
-        <div className="book-cover" />
+      <div className="book-cover">
+        <img src="https://marketplace.canva.com/EADanktU9AE/1/0/251w/canva-green-beach-photo-book-cover-o2wPCwYqW2w.jpg" alt="image of something" />
+      </div>
+
+      <div class="book-description">
+        <div className="book-title">1776</div>
+        <div className="book-authors">David McCullough</div>
         <div className="book-shelf-changer">
-          <select>
-            <option value="move" disabled>
-              Move to...
-            </option>
+          <select className="select">
             <option value="currentlyReading">Currently Reading</option>
             <option value="wantToRead">Want to Read</option>
             <option value="read">Read</option>
@@ -18,8 +20,6 @@ function BookShelfItem(props) {
           </select>
         </div>
       </div>
-      <div className="book-title">1776</div>
-      <div className="book-authors">David McCullough</div>
     </div>
   )
 }
